d2c18f70afde1a1aeae01a40550f74ac58cf5741
scott hain <shain@anaconda.com>
2025-04-01 05:57:05 -0700

    Pull more conda-forge updates in
    Signed-off-by: scott hain <shain@anaconda.com>

--- recipe/install-clangxx.sh
+++ recipe/install-clangxx.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
 
 set -e -x
+mkdir -p "${PREFIX}"/etc/conda/{de,}activate.d/
+cp "${SRC_DIR}"/activate-clang++.sh "${PREFIX}"/etc/conda/activate.d/activate_"${PKG_NAME}".sh
+cp "${SRC_DIR}"/deactivate-clang++.sh "${PREFIX}"/etc/conda/deactivate.d/deactivate_"${PKG_NAME}".sh
 
-CHOST=${macos_machine}
-echo CHOST is ${CHOST}
+# #!/bin/bash
 
-pushd "${PREFIX}"/bin
-  ln -s clang++ ${CHOST}-clang++
-  if [[ "${CHOST}" != "${CBUILD}" ]] && [[ "${target_platform}" != linux-* || ${version} == "15.0.7" ]]; then
-    # on linux, the `clangxx` package already has a $TRIPLE-clang++, see
-    # https://github.com/conda-forge/clangdev-feedstock/pull/251
-    ln -s clang++ ${CBUILD}-clang++
-  fi
-popd
+# set -e -x
+
+# CHOST=${macos_machine}
+# echo CHOST is ${CHOST}
+
+# pushd "${PREFIX}"/bin
+#   ln -s clang++ ${CHOST}-clang++
+#   if [[ "${CHOST}" != "${CBUILD}" ]] && [[ "${target_platform}" != linux-* || ${version} == "15.0.7" ]]; then
+#     # on linux, the `clangxx` package already has a $TRIPLE-clang++, see
+#     # https://github.com/conda-forge/clangdev-feedstock/pull/251
+#     ln -s clang++ ${CBUILD}-clang++
+#   fi
+# popd
