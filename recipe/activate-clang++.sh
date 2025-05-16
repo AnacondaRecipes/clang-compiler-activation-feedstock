8b6e079f4875c1d919316d0533cedccb14a66635
Eric Lundby <Eric.Lundby@gmail.com>
2025-05-16 13:50:28 -0600

    Tweak flags to fix cctools also.

--- recipe/activate-clang++.sh
+++ recipe/activate-clang++.sh
@@ -82,13 +82,13 @@ function _tc_activation() {
 function activate_clangxx() {
 # When people are using conda-build, assume that adding rpath during build, and pointing at
 #    the host env's includes and libs is helpful default behavior
+MAJOR_VERSION=$(echo ${PKG_VERSION} | cut -f1 -d".")
 if [ "${CONDA_BUILD:-0}" = "1" ]; then
-  MAJOR_VERSION=$(echo ${PKG_VERSION} | cut -f1 -d".")
-  CXXFLAGS_USED="@CXXFLAGS@ -I${BUILD_PREFIX}/lib/clang/${MAJOR_VERSION}/include -isystem ${PREFIX}/include -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix"
-  DEBUG_CXXFLAGS_USED="@CXXFLAGS@ @DEBUG_CXXFLAGS@ -I${BUILD_PREFIX}/lib/clang/${MAJOR_VERSION}/include -isystem ${PREFIX}/include -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix"
+  CXXFLAGS_USED="@CXXFLAGS@ -I${BUILD_PREFIX}/lib/clang/${MAJOR_VERSION}/include -I${PREFIX}/include/c++/v1 -isystem ${PREFIX}/include -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix"
+  DEBUG_CXXFLAGS_USED="@CXXFLAGS@ @DEBUG_CXXFLAGS@ -I${BUILD_PREFIX}/lib/clang/${MAJOR_VERSION}/include -I${PREFIX}/include/c++/v1 -isystem ${PREFIX}/include -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix"
 else
-  CXXFLAGS_USED="@CXXFLAGS@ -isystem ${CONDA_PREFIX}/include"
-  DEBUG_CXXFLAGS_USED="@CXXFLAGS@ @DEBUG_CXXFLAGS@ -isystem ${CONDA_PREFIX}/include"
+  CXXFLAGS_USED="@CXXFLAGS@ -I${CONDA_PREFIX}/lib/clang/${MAJOR_VERSION}/include -I${CONDA_PREFIX}/include/c++/v1 -isystem ${CONDA_PREFIX}/include"
+  DEBUG_CXXFLAGS_USED="@CXXFLAGS@ @DEBUG_CXXFLAGS@ -I${CONDA_PREFIX}/lib/clang/${MAJOR_VERSION}/include -I${CONDA_PREFIX}/include/c++/v1 -isystem ${CONDA_PREFIX}/include"
 fi
 
 if [ "${CONDA_BUILD:-0}" = "1" ]; then
