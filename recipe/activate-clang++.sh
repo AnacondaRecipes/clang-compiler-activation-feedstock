934ca6391c3fe922c9deec63a8e81d436a0d0a74
Eric Lundby <Eric.Lundby@gmail.com>
2025-05-28 17:11:41 -0600

    Add back minimum CXXFLAGS

--- recipe/activate-clang++.sh
+++ recipe/activate-clang++.sh
@@ -84,7 +84,7 @@ function activate_clangxx() {
 #    the host env's includes and libs is helpful default behavior
 MAJOR_VERSION=$(echo ${PKG_VERSION} | cut -f1 -d".")
 if [ "${CONDA_BUILD:-0}" = "1" ]; then
-  CXXFLAGS_USED="@CXXFLAGS@ -isystem ${PREFIX}/include -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix"
+  CXXFLAGS_USED="@CXXFLAGS@ -isystem ${CONDA_BUILD_SYSROOT}/usr/include -isystem ${PREFIX}/include -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix"
   DEBUG_CXXFLAGS_USED="@CXXFLAGS@ @DEBUG_CXXFLAGS@ -isystem ${PREFIX}/include -fdebug-prefix-map=${SRC_DIR}=/usr/local/src/conda/${PKG_NAME}-${PKG_VERSION} -fdebug-prefix-map=${PREFIX}=/usr/local/src/conda-prefix"
 else
   CXXFLAGS_USED="@CXXFLAGS@ -I${CONDA_PREFIX}/lib/clang/${MAJOR_VERSION}/include -I${CONDA_PREFIX}/include/c++/v1 -isystem ${CONDA_PREFIX}/include"
